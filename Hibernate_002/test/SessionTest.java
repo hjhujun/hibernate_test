60771c463c31a337beb967b778f769a592b1c000
hjhujun <hjhujun@hotmail.com>
2017-03-02 17:25:10 +0800

    1

--- Hibernate_002/test/SessionTest.java
+++ Hibernate_002/test/SessionTest.java
@@ -1,7 +1,13 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Date;
 
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -14,141 +20,77 @@ import org.junit.Test;
 public class SessionTest {
 
 	@Test
-	public void testOpensession(){
-		//获得配置对象
+	public void testOpensession() {
+		// 获得配置对象
 		Configuration config = new Configuration().configure();
-		//获得服务注册对象
-		//ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
-		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
-		//获得sessionFactory对象
+		// 获得服务注册对象
+		// ServiceRegistry serviceRegistry = new
+		// StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties())
+				.build();
+		// 获得sessionFactory对象
 		SessionFactory sessionFactory = config.buildSessionFactory();
-		//获得session对象
+		// 获得session对象
 		Session session = sessionFactory.openSession();
 		Session session1 = sessionFactory.openSession();
 		Session session2 = sessionFactory.openSession();
-		System.out.println(session1==session2);
-		if(session != null){
+		System.out.println(session1 == session2);
+		if (session != null) {
 			System.out.println("open session 创建成功！");
-		}
-		else {
+		} else {
 			System.out.println("open session 创建失败");
 		}
-		
-	}
-	
-	
-	@Test
-	public void testGetCurrentSession(){
-		//获得配置对象
-		Configuration config = new Configuration().configure();
-		
-		//获得服务注册对象
-		//ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
 
-		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
-		
-		//获得sessionFactory对象
+	}
+
+	@Test
+	public void testGetCurrentSession() {
+		// 获得配置对象
+		Configuration config = new Configuration().configure();
+
+		// 获得服务注册对象
+		// ServiceRegistry serviceRegistry = new
+		// StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties())
+				.build();
+
+		// 获得sessionFactory对象
 		SessionFactory sessionFactory = config.buildSessionFactory();
-		
-		//获得session对象
+
+		// 获得session对象
 		Session session = sessionFactory.getCurrentSession();
 		Session session1 = sessionFactory.getCurrentSession();
 		Session session2 = sessionFactory.getCurrentSession();
-		System.out.println(session1==session2);
+		System.out.println(session1 == session2);
 
-		if(session != null){
+		if (session != null) {
 			System.out.println("get current session 创建成功！");
-		}
-		else {
+		} else {
 			System.out.println("get current session 创建失败");
 		}
-		
-	}
-	
-	@Test
-	public void testSaveStudentsWithOpenSeeion()
-	{
-		//获得配置对象
-				Configuration config = new Configuration().configure();
-				//获得服务注册对象
-				//ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
-				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
-				//获得sessionFactory对象
-				SessionFactory sessionFactory = config.buildSessionFactory();
-				//获得session对象
-				Session session1 = sessionFactory.openSession();
-				//开启事务
-				Transaction transcation = session1.beginTransaction();
-				//生成一个学生对象
-				Students s1 = new  Students(8, "ali88", "man", new Date(), "beijing");
-				
-				session1.doWork(new Work(){
-					@Override
-					public void execute(Connection connection) throws SQLException {
-						// TODO Auto-generated method stub
-						System.out.println("session1 has code is " + connection.hashCode());
 
-					}
-				});
-				
-				session1.save(s1);
-				//session1.close();
-				transcation.commit();
-				
-				Session session2 = sessionFactory.openSession();
-				transcation = session2.beginTransaction();
-				
-				session2.doWork(new Work(){
-					@Override
-					public void execute(Connection connection) throws SQLException {
-						// TODO Auto-generated method stub
-						System.out.println("session2 has code is " + connection.hashCode());
-
-					}
-				});
-				
-				Students s2 = new  Students(7, "ali77", "man", new Date(), "beijing");
-				session2.save(s2);
-				//session2.close();
-				transcation.commit();
 	}
-	
+
 	@Test
-	public void testSaveStudentsWithGetCurrentSession(){
-		try {
-			
-		
-		//获得配置对象
+	public void testSaveStudentsWithOpenSeeion() {
+		// 获得配置对象
 		Configuration config = new Configuration().configure();
-		
-		//获得服务注册对象
-		//ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
-
-		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
-		
-		//获得sessionFactory对象
+		// 获得服务注册对象
+		// ServiceRegistry serviceRegistry = new
+		// StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties())
+				.build();
+		// 获得sessionFactory对象
 		SessionFactory sessionFactory = config.buildSessionFactory();
-		
-		//获得session对象
-		//Session session = sessionFactory.getCurrentSession();
-		Session session1 = sessionFactory.getCurrentSession();
-		
-		//开启事务
+		// 获得session对象
+		Session session1 = sessionFactory.openSession();
+		// 开启事务
 		Transaction transcation = session1.beginTransaction();
-		
-//		System.out.println(session1==session2);
+		// 生成一个学生对象
+		Students s1 = new Students(8, "ali88", "man", new Date(), "beijing");
 
-//		if(session != null){
-//			System.out.println("get current session 创建成功！");
-//		}
-//		else {
-//			System.out.println("get current session 创建失败");
-//		}
-		
-		
-		Students s1 = new  Students(8, "ali88", "man", new Date(), "beijing");
-		
-		session1.doWork(new Work(){
+		session1.doWork(new Work() {
 			@Override
 			public void execute(Connection connection) throws SQLException {
 				// TODO Auto-generated method stub
@@ -156,20 +98,15 @@ public class SessionTest {
 
 			}
 		});
-		
-		session1.save(s1);
-		transcation.commit();
-		//session1.close();
-		
-		
-		
-		
-	//	Session session2 = sessionFactory.openSession();
-		Session session2 = sessionFactory.getCurrentSession();
 
+		session1.save(s1);
+		// session1.close();
+		transcation.commit();
+
+		Session session2 = sessionFactory.openSession();
 		transcation = session2.beginTransaction();
-		
-		session2.doWork(new Work(){
+
+		session2.doWork(new Work() {
 			@Override
 			public void execute(Connection connection) throws SQLException {
 				// TODO Auto-generated method stub
@@ -177,17 +114,139 @@ public class SessionTest {
 
 			}
 		});
-		
-		Students s2 = new  Students(7, "ali77", "man", new Date(), "beijing");
+
+		Students s2 = new Students(7, "ali77", "man", new Date(), "beijing");
 		session2.save(s2);
-		//session2.close();
+		// session2.close();
 		transcation.commit();
+	}
+
+	@Test
+	public void testSaveStudentsWithGetCurrentSession() {
+		try {
+
+			// 获得配置对象
+			Configuration config = new Configuration().configure();
+
+			// 获得服务注册对象
+			// ServiceRegistry serviceRegistry = new
+			// StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+
+			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties())
+					.build();
+
+			// 获得sessionFactory对象
+			SessionFactory sessionFactory = config.buildSessionFactory();
+
+			// 获得session对象
+			// Session session = sessionFactory.getCurrentSession();
+			Session session1 = sessionFactory.getCurrentSession();
+
+			// 开启事务
+			Transaction transcation = session1.beginTransaction();
+
+			// System.out.println(session1==session2);
+
+			// if(session != null){
+			// System.out.println("get current session 创建成功！");
+			// }
+			// else {
+			// System.out.println("get current session 创建失败");
+			// }
+
+			Students s1 = new Students(8, "ali88", "man", new Date(), "beijing");
+
+			session1.doWork(new Work() {
+				@Override
+				public void execute(Connection connection) throws SQLException {
+					// TODO Auto-generated method stub
+					System.out.println("session1 has code is " + connection.hashCode());
+
+				}
+			});
+
+			session1.save(s1);
+			transcation.commit();
+			// session1.close();
+
+			// Session session2 = sessionFactory.openSession();
+			Session session2 = sessionFactory.getCurrentSession();
+
+			transcation = session2.beginTransaction();
+
+			session2.doWork(new Work() {
+				@Override
+				public void execute(Connection connection) throws SQLException {
+					// TODO Auto-generated method stub
+					System.out.println("session2 has code is " + connection.hashCode());
+
+				}
+			});
+
+			Students s2 = new Students(7, "ali77", "man", new Date(), "beijing");
+			session2.save(s2);
+			// session2.close();
+			transcation.commit();
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.println(e);
 		}
 	}
-		
-	
-}
 
+	@Test
+	public void testWriteBlob() {
+		try {
+
+			// 获得配置对象
+			Configuration config = new Configuration().configure();
+
+			// 获得服务注册对象
+			// ServiceRegistry serviceRegistry = new
+			// StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+
+			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties())
+					.build();
+
+			// 获得sessionFactory对象
+			SessionFactory sessionFactory = config.buildSessionFactory();
+
+			// 获得session对象
+			// Session session = sessionFactory.getCurrentSession();
+			Session session = sessionFactory.openSession();
+			Transaction transcation = session.beginTransaction();
+
+			System.out.println("testWriteBlob");
+
+			Students s = new Students(999, "name", "gender", new Date(), "address");
+
+			File f = new File("C:\\reskill\\ipcontrol.jpg");
+
+			InputStream input = new FileInputStream(f);
+			// byte[] bFile = new byte[f];
+			System.out.println(input.available());
+			System.out.println(input);
+			Blob image = Hibernate.getLobCreator(session).createBlob(input, input.available());
+			// Hibernate.getLobCreator(session).createBlob(input,
+			// input.available());
+
+			System.out.println(input.available());
+
+			s.setPicture(image); // (image);
+
+			session.save(s);
+			// 提交事务
+			transcation.commit();
+
+			// 关闭会话
+			session.close();
+
+			// 关闭会话工厂
+			sessionFactory.close();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
+	}
+
+}
--- Hibernate_002/test/StudentsTest.java
+++ Hibernate_002/test/StudentsTest.java
@@ -1,5 +1,4 @@
 
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -29,140 +28,133 @@ public class StudentsTest {
 	private SessionFactory sessionFactory;
 	private Session session;
 	private Transaction transaction;
-	
+
 	@Before
-	public void Init()
-	{
+	public void Init() {
 		try {
 			System.out.println("init");
-			
-//			//获得配置对象
-//			Configuration config = new Configuration().configure();
-//			//获得服务注册对象
-//			//ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
-//			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
-//			//获得sessionFactory对象
-//			SessionFactory sessionFactory = config.buildSessionFactory();
-//			//获得session对象
-//			Session session = sessionFactory.openSession();
-//			
-//			
-			
-			
-			
-			
 
-			//创建配置对象
+			// //获得配置对象
+			// Configuration config = new Configuration().configure();
+			// //获得服务注册对象
+			// //ServiceRegistry serviceRegistry = new
+			// StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+			// ServiceRegistry serviceRegistry = new
+			// StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+			// //获得sessionFactory对象
+			// SessionFactory sessionFactory = config.buildSessionFactory();
+			// //获得session对象
+			// Session session = sessionFactory.openSession();
+			//
+			//
+
+			// 创建配置对象
 			Configuration config = new Configuration().configure();
-			//创建服务注册对象
-			//ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
-			//创建会话工厂对象
-			//sessionFactory = config.buildSessionFactory(serviceRegistry);
+			// 创建服务注册对象
+			// ServiceRegistry serviceRegistry = new
+			// StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+			// 创建会话工厂对象
+			// sessionFactory = config.buildSessionFactory(serviceRegistry);
 			sessionFactory = config.buildSessionFactory();
-			//会话对象
+			// 会话对象
 			session = sessionFactory.openSession();
-			//开启事务。
+			// 开启事务。
 			transaction = session.beginTransaction();
-			
+
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.println(e);
 		}
-		
+
 	}
+
 	@After
-	public void destory()
-	{
+	public void destory() {
 		try {
 			System.out.println("destory");
-			
-			//提交事务
+
+			// 提交事务
 			transaction.commit();
-			
-			//关闭会话
+
+			// 关闭会话
 			session.close();
-			
-			//关闭会话工厂
+
+			// 关闭会话工厂
 			sessionFactory.close();
-			
+
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.println(e);
 		}
-		
-		
-		
+
 	}
-	
+
 	@Test
-	public void teststudent()
-	{
+	public void teststudent() {
 		try {
 			System.out.println("teststudent");
 
-			//生成学生对象
-			//Students s = new Students(3,"qqq","qw", new Date(), "ccc");
+			// 生成学生对象
+			// Students s = new Students(3,"qqq","qw", new Date(), "ccc");
 			Students s = new Students();
 			s.setAddress("wudangshan");
 			s.setBirthday(new Date());
 			s.setGender("man");
 			s.setSname("myname");
 			s.setStid(2);
-			//s.setPicture(null);
-			//保存对象 写数据库
+			// s.setPicture(null);
+			// 保存对象 写数据库
 			session.save(s);
-			
+
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.out.println(e);
 		}
-		
-		
-	}
-	
-	@Test
-	public void  testWriteBlob() throws IOException  {
-		
-		System.out.println("testWriteBlob");
-			
-		Students s = new Students(1, "name", "gender", new Date(), "address");
-		
-		File f = new File("C:\\reskill\\ipcontrol.jpg");
-		
-		InputStream input = new FileInputStream(f);
-		//byte[] bFile = new byte[f];
-		  System.out.println(input.available());
-System.out.println(input);
-		Blob image = Hibernate.getLobCreator(session).createBlob(input,input.available() ); 
-				//Hibernate.getLobCreator(session).createBlob(input, input.available());
-		  
-		  System.out.println(input.available());
 
-		s.setPicture(image); //(image);
-		
+	}
+
+	@Test
+	public void testWriteBlob() throws IOException {
+
+		System.out.println("testWriteBlob");
+
+		Students s = new Students(1, "name", "gender", new Date(), "address");
+
+		File f = new File("C:\\reskill\\ipcontrol.jpg");
+
+		InputStream input = new FileInputStream(f);
+		// byte[] bFile = new byte[f];
+		System.out.println(input.available());
+		System.out.println(input);
+		Blob image = Hibernate.getLobCreator(session).createBlob(input, input.available());
+		// Hibernate.getLobCreator(session).createBlob(input,
+		// input.available());
+
+		System.out.println(input.available());
+
+		s.setPicture(image); // (image);
 
 		session.save(s);
 	}
-	
+
 	@Test
-	public void testReadBlob() throws Exception{
+	public void testReadBlob() throws Exception {
 		Students s = (Students) session.get(Students.class, 1);
-		
+
 		Blob image = s.getPicture();
-		
+
 		InputStream inputStream = image.getBinaryStream();
-		
+
 		File f = new File("C:\\reskill\\getpic.jpg");
-		
+
 		OutputStream output = new FileOutputStream(f);
-		
+
 		byte[] buff = new byte[inputStream.available()];
 		inputStream.read(buff);
 		output.write(buff);
 
 		inputStream.close();
 		output.close();
-		
-		
+
 	}
 }
